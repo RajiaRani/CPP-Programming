c506f34578c4f999c1193a8af8ff5065da394836
Rajia Rani <rajiarani@Chenchus-MacBook-Pro.local>
2023-11-11 15:39:51 -0600

    updated the file

--- 1.substracta.cpp
+++ 1.substracta.cpp
@@ -1,9 +1,15 @@
+//basic substraction using c++
 #include<iostream>
 #include<math.h>
 using namespace std;
 int main()
 {
-	int x=34,y=23;
-	cout<<x<<"-"<<y<<"="<<x-y<<endl;
+	//take any two or maore than two any rendom number
+	int x = 34;
+	int y = 23;
+    
+  //you can use this direct output function
+   cout<< x <<" - "<< y <<" = "<< x-y <<endl; 
+
 	return 0;
 }
