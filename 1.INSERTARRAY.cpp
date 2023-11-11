ea8d006db955cf356abf9b543cd539d0613e774c
Rajia Rani <rajiarani@Chenchus-MacBook-Pro.local>
2023-11-11 15:33:39 -0600

    updated the file with new code

--- 1.INSERTARRAY.cpp
+++ 1.INSERTARRAY.cpp
@@ -2,18 +2,25 @@
 using namespace std;
 int main()
 {
-	int a[6],num,i;
+	int arr[6];
+	int num,i;
 	cout<<"Enter any 5 array number "<<endl;
 	for(int i=0; i<5;i++)
-	cin>>a[i];
+	{
+		cin>>arr[i];
+	}
+
 	cout<<"Enter any number to insert"<<endl;
 	cin>>num;
-	a[5]=num;
+	//add the new number to the last of the array
+	arr[5]=num;
 
 	cout<<" The new array is "<<endl;
 	for(int i=0;i<6;i++)
-	
-	cout<<a[i]<<"   "<<endl;
+     {
+	  cout<<arr[i]<<"   "<<endl;
+	 }
 	return 0;
-	
+
+	// updated the file
 }
